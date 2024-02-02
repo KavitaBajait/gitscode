6354805f698e99b45540820a058fc7dc3dcb4d6f
KavitaBajait <kavitabajait01@gmail.com>
2024-02-02 23:40:50 +0530

    data types

---
+++ Datatype.js
@@ -0,0 +1,22 @@
+console.log('kdkd');
+
+// 1. string (to store alphanumeric value)
+// 2. number
+// 3. bigint
+// 4. boolean
+// 5. undefined
+// 6. null
+// 7. symbol
+// 8. object
+
+// The object data types
+// The object data types can contain:
+
+// 1. An object
+// 2. An array
+// 3. A date
+
+
+let state = ""; //empty string
+let city = "pune"; //string data type
+console.log(city);
---
+++ variable.js
@@ -0,0 +1,22 @@
+//variable: are the container to store the data (data type)
+//let/var/const default var(keyword) followed by variable name
+//VN : rule>>> space not allowed, 
+//first ltr of variable should always alphabets, 
+//special char not allowed(except $ _)
+
+// let myFirstName = 'kavita';
+
+let fName; //declaration of variable //undefined
+
+fName = 'kavita'; //declaration (syntax variableName = value/date) //difference =(assingment operator) == ===
+
+let myFirstname = 'kavita'; //declr + defination
+
+var mySName= 'bajait';
+var myName;
+myName ='dhiraj';
+myName ='Gunjan'
+
+const cityName = 'pune'; //const keyword initialization of value is must
+//cityName ='wardha'
+console.log('cityName',cityName);
